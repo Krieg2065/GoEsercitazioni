1e18d3f1f0393bcc6fe8e9fe8d448da4ca7da27c
Krieg2065 <ismail.mohamed@gmail.com>
2023-10-11 16:04:01 +0200

    sistemato es7

--- es7/convertisec.go
+++ es7/convertisec.go
@@ -5,7 +5,10 @@ func main(){
 	var s int
 	fmt.Print("Secondi? ")
 	fmt.Scan(&s)
-	h,m := s/60/60,s/60
-	stri := fmt.Sprintf("h:m:sec - %d:%d:%d",h,m,s)
+	h := s/60/60
+	m :=s/60 - h*60
+	sec := s - h*60*60 - m * 60
+	
+	stri := fmt.Sprintf("h:m:sec - %d:%d:%d",h,m,sec)
 	fmt.Println(stri)
 }
